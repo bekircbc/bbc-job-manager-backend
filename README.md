255a53765d85dfeb6ad045ba6997fc7301ac36fe
Bekir - CDI <b.burakcebecii@gmail.com>
2022-07-12 12:07:18 +0200

    /job-sources-local is ready to use..

---
+++ README.md
@@ -0,0 +1,30 @@
+# Connection between MongoDB and server.js
+
+- models/JobSource.js
+
+        import mongoose from "mongoose";
+
+        const jobSourceSchema = new mongoose.Schema({
+         name: String,
+          url: String,
+        });
+
+        export const JobSource = mongoose.model("job-source", jobSourceSchema);
+
+- server.js
+
+        const MONGODB_URI222 = "mongodb://localhost/bbc-job-manager";
+
+        mongoose.connect(MONGODB_URI222, (err) => {
+         if (err) {
+         console.log({
+        error: "Cannot connect to MongoDB database.",
+          err: `"${err}"`,
+         });
+          }
+        });
+
+        app.get("/job-sources-local", async (req, res) => {
+        const jobSources = await JobSource.find();
+        res.status(200).json({ message: "fetching data from local", jobSources });
+        });
--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 import express from "express";
-import { JobSource } from "./models/JobSource";
+import { JobSource } from "./models/JobSource.js";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 
 dotenv.config();
 
-const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/bbc-job-manager";
+// const MONGODB_URI =
+//   process.env.MONGODB_URI || "mongodb://localhost/bbc-job-manager";
 
-mongoose.connect(MONGODB_URI, (err) => {
+//   mongoose.connect(MONGODB_URI, (err) => {
+//     if (err) {
+//       console.log({
+//         error: "Cannot connect to MongoDB database.",
+//         err: `"${err}"`,
+//       });
+//     }
+//   });
+
+const MONGODB_URI222 = "mongodb://localhost/bbc-job-manager";
+
+mongoose.connect(MONGODB_URI222, (err) => {
   if (err) {
     console.log({
       error: "Cannot connect to MongoDB database.",
@@ -23,6 +34,16 @@ const port = process.env.PORT || 30445;
 app.get("/", (req, res) => {
   res.send("<h1>Job Manager API</h1>");
 });
+
+app.get("/job-sources-local", async (req, res) => {
+  const jobSources = await JobSource.find();
+  res.status(200).json({ message: "fetching data from local", jobSources });
+});
+
+// app.get("/job-sources-online", async (req, res) => {
+//   const jobSources = await JobSource.find();
+//   res.status(200).json({ message: "fetching data from online", jobSources });
+// });
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
