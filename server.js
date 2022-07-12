b26c7aa704ff4ae08fc949ca58103e21087fbd57
Bekir - CDI <b.burakcebecii@gmail.com>
2022-07-12 13:37:31 +0200

    getting data from online database.. and working..

--- server.js
+++ server.js
@@ -5,18 +5,6 @@ import mongoose from "mongoose";
 
 dotenv.config();
 
-// const MONGODB_URI =
-//   process.env.MONGODB_URI || "mongodb://localhost/bbc-job-manager";
-
-//   mongoose.connect(MONGODB_URI, (err) => {
-//     if (err) {
-//       console.log({
-//         error: "Cannot connect to MongoDB database.",
-//         err: `"${err}"`,
-//       });
-//     }
-//   });
-
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/bbc-job-manager";
 
@@ -36,15 +24,10 @@ app.get("/", (req, res) => {
   res.send("<h1>Job Manager API</h1>");
 });
 
-app.get("/job-sources-local", async (req, res) => {
+app.get("/job-sources", async (req, res) => {
   const jobSources = await JobSource.find();
   res.status(200).json({ message: "fetched data from local", jobSources });
 });
-
-// app.get("/job-sources-online", async (req, res) => {
-//   const jobSources = await JobSource.find();
-//   res.status(200).json({ message: "fetching data from online", jobSources });
-// });
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
