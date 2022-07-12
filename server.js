c0667673ddf3aff8d18916dd96e530b85e0ac0bb
Bekir - CDI <b.burakcebecii@gmail.com>
2022-07-12 11:25:37 +0200

    added mongodb and error. created also local database..

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 import express from "express";
+import { JobSource } from "./models/JobSource";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/bbc-job-manager";
+
+mongoose.connect(MONGODB_URI, (err) => {
+  if (err) {
+    console.log({
+      error: "Cannot connect to MongoDB database.",
+      err: `"${err}"`,
+    });
+  }
+});
 
 const app = express();
 const port = process.env.PORT || 30445;
